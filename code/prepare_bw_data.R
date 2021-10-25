fe1950a7acbee5c6f0b6d5534f010629a584ecdf
Øystein Sørensen <oyss@medisin.uio.no>
2021-10-25 13:13:23 +0200

    Started on workflow

--- code/prepare_bw_data.R
+++ code/prepare_bw_data.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# Birthweight data downloaded from Wolfram Research, "Sample Data: Birth Weight Risk" from the Wolfram Data Repository (2016) https://doi.org/10.24097/wolfram.85529.data
+# Birthweight data downloaded from Wolfram Research, "Sample Data: Birth Weight Risk" from the Wolfram Data Repository (2016) https://doi.org/10.24097/wolfram.85529.data
 
 dat <- read_csv("data/Sample-Data-Birth-Weight-Risk.csv")
 
@@ -10,4 +10,4 @@ dat %>%
     across(c(Age, LWT), ~ as.numeric(str_extract(., "[:digit:]+"))),
     LWT = 0.45359237 * LWT
   ) %>% 
-  write_csv("data/bwt.csv")
+  saveRDS("data/bwt.rds")
