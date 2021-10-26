3040312aef9c5af8dee466d192952a82abf4fc61
Øystein Sørensen <oyss@medisin.uio.no>
2021-10-26 21:46:49 +0200

    Worked through part 2

--- code/bw_workflow.R
+++ code/bw_workflow.R
@@ -25,3 +25,15 @@ ggsave("figures/bad_trace_plot_example.png", width = 12, height = 6, units = "cm
 rm(models)
 
 
+pp_check(mod, ndraws = 100, type = "bars")
+ggsave("figures/pp_bar_example.png", width = 8, height = 6, units = "cm")
+
+pp_check(mod, ndraws = 9, type = "error_binned")
+ggsave("figures/pp_error_binned_example.png", width = 12, height = 8, units = "cm")
+
+mod2 <- brm(Low ~ LWT + Smoker, data = dat, family = bernoulli(),
+            prior = set_prior("normal(0, 0.4)", "b", coef = "LWT"))
+
+waic(mod, mod2, model_names = c("LWT", "LWT+Smoker"))
+loo_compare(loo(mod), loo(mod2))
+
