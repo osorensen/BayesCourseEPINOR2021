ddec62fca436b74d9214e6a2794a5dbfc3701214
Øystein Sørensen <oyss@medisin.uio.no>
2021-10-28 14:50:30 +0200

    Updated

--- create_files.R
+++ create_files.R
@@ -2,6 +2,6 @@ library(tidyverse)
 library(glue)
 files <- list.files(pattern = "part.*Rmd")
 walk(files, function(f){
-  rmarkdown::render(f, output_format = "html_document", 
-                    output_file = glue("docs/{str_replace(f, 'Rmd', 'html')}"))
+  rmarkdown::render(f, output_format = "xaringan::moon_reader", 
+                    output_file = glue("{str_replace(f, 'Rmd', 'html')}"))
   })
